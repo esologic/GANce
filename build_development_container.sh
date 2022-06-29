405707af0a792ead472bcb7417dc484d48c6f1aa
Devon Bray <dev@esologic.com>
2022-06-29 09:21:39 -0400

    Improved development container build script

--- build_development_container.sh
+++ build_development_container.sh
@@ -4,5 +4,5 @@
 # Locates the `:x` of the currently attached to display to be able to pass this to the container,
 # so it can be written to
 
-sudo docker-compose build --build-arg DISPLAY="$(w -hs | grep gdm | awk '{print $3}' | head -n 1)" gance-develop
+sudo docker-compose build --build-arg DISPLAY="$(w -hs | grep '/usr/lib/gdm3/gdm-x-session' | awk '{print $3}' | head -n 1)" gance-develop
 sudo docker-compose up --detach gance-develop
